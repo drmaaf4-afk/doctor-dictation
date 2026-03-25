a612cda267c743429870fb0c9711c67a35413f49
drmaaf4-afk <drmaaf4@gmail.com>
2026-03-25 16:02:21 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Arial'
+    }}>
+      <div style={{
+        padding: 30,
+        borderRadius: 12,
+        boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+        textAlign: 'center'
+      }}>
+        <h1>Doctor Dictation System</h1>
+        <p>Step 1 ready 🚀</p>
+      </div>
+    </main>
+  )
+}
